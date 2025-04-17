c29617875af70e91352d177e55952976f6142f58
tygszfa <185154684+genizy@users.noreply.github.com>
2025-04-17 14:21:55 -0700

    fix controls

--- CrazyCattle3D.service.worker.js
+++ CrazyCattle3D.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1744924564|4790188404';
+const CACHE_VERSION = '1744924886|105187275';
 /** @type {string} */
 const CACHE_PREFIX = 'CrazyCattle3D-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
